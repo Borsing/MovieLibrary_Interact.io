d7c081fa80d7f52d0b1f6505178af68816cba675
Benjamin Robert <benji_robert@msn.com>
2017-04-11 22:00:56 +0200

    [fix] Add } missing

--- src/main/java/com/benjamin/interact/movieLibrary/entity/Movie.java
+++ src/main/java/com/benjamin/interact/movieLibrary/entity/Movie.java
@@ -31,11 +31,12 @@ public class Movie {
 
     /**
      * Private constructor to create a movie entity.
-     * @param id of the movie
-     * @param title of the movie
+     *
+     * @param id          of the movie
+     * @param title       of the movie
      * @param releaseYear of the movie
      */
-    private Movie(UUID id, String title, int releaseYear){
+    private Movie(UUID id, String title, int releaseYear) {
         this.id = id;
         this.title = title;
         this.releaseYear = releaseYear;
@@ -44,49 +45,57 @@ public class Movie {
     /**
      * Public constructor to create a movie entity.
      * The constructor generate a randomUUID for each movie.
-     * @param title of the movie
+     *
+     * @param title       of the movie
      * @param releaseYear of the movie
      */
-    public Movie(String title, int releaseYear){
+    public Movie(String title, int releaseYear) {
         this(UUID.randomUUID(), title, releaseYear);
     }
 
     /**
      * Accessor to the Id
+     *
      * @return id of the movie
      */
-    public UUID getId(){
-        return this.id ;
+    public UUID getId() {
+        return this.id;
     }
 
     /**
      * Accessor to the title
+     *
      * @return title of the movie
      */
-    public String getTitle(){
-        return this.title ;
+    public String getTitle() {
+        return this.title;
     }
 
     /**
      * Accessor to the release year
+     *
      * @return the release year of the movie
      */
-    public int getReleaseDate(){
-        return this.releaseYear ;
+    public int getReleaseDate() {
+        return this.releaseYear;
     }
 
     /**
      * Setter for the title of the movie
+     *
      * @param title of the movie
      */
-    public void setTitle(String title){
-        this.title =title;
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     /**
      * Setter for the release year
+     *
      * @param releaseYear of the movie
      */
-    public void setReleaseDate(int releaseYear){
+    public void setReleaseDate(int releaseYear) {
         this.releaseYear = releaseYear;
     }
+
+}
